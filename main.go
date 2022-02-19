22ebfcc8e2b41d003f41bc9696052cf3e4d116fc
eremin.is2 <ivan.eremin@domru.ru>
2022-02-19 14:04:37 +0700

    fix

--- main.go
+++ main.go
@@ -1,12 +1,11 @@
-package main
+package square
 
 import (
 	"fmt"
-	"golang-united-school-homework-2/square"
 )
 
 func main() {
-	fmt.Println("Площать круга: ", square.CalcSquare(10.0, square.SidesCircle))
-	fmt.Println("Площать треугольника: ", square.CalcSquare(10.0, square.SidesTriangle))
-	fmt.Println("Площать квадрата: ", square.CalcSquare(10.0, square.SidesSquare))
+	fmt.Println("Площать круга: ", CalcSquare(10.0, SidesCircle))
+	fmt.Println("Площать треугольника: ", CalcSquare(10.0, SidesTriangle))
+	fmt.Println("Площать квадрата: ", CalcSquare(10.0, SidesSquare))
 }
